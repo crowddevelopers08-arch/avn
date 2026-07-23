4d25cbec833d9a6ae3fb9575ce6a727f3c9f31db
fenil matt fenil matt <ffenilmatt@gmail.com>
2026-07-23 13:24:20 +0530

    gtagsnewhareesh

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,12 +46,27 @@ export default function RootLayout({
       <head>
         {/* Google Tag Manager Script */}
         <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MKSQV9HN');
+            `,
+          }}
+        />
+
+        {/* Google Ads Conversion Tracking */}
+        <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=AW-947598506"
           strategy="afterInteractive"
         />
         <Script
-          id="google-analytics"
+          id="google-ads"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
@@ -89,6 +104,16 @@ export default function RootLayout({
           src="https://crm.zoho.in/crm/javascript/zcga.js"
           strategy="afterInteractive"
         />
+
+        {/* Google Tag Manager Noscript - placed here for head, but ideally should be in body */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MKSQV9HN"
+            height="0" 
+            width="0" 
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
       </head>
       <body>{children}</body>
     </html>
